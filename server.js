388c8313f598a935b1d835d7d2be0b73d06ab938
Akshat <akshatvishwakarma958@gmail.com>
2023-08-10 15:44:55 +0530

    adding the server commit

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const serverconfig = require('./configs/server.configs')
 
 
 const app = express();
 
 
-app.listen(3333, ()=>{
+app.listen(serverconfig.PORT, ()=>{
 
-    console.log("server started");
+    console.log('server started on the port number ${serverconfig.PORT}' );
 }
 
 
